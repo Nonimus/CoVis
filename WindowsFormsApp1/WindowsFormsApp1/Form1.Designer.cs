c80da091e3a1ace4f5b3848e60f3cb2ede319123
Nonimus <f.swimm@web.de>
2018-05-03 16:01:53 +0200

    erster commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.showButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.BChooseFile = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.PicNumberTaker = new System.Windows.Forms.NumericUpDown();
+            this.BGTeinlesen = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicNumberTaker)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.16817F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.83183F));
+            this.tableLayoutPanel1.Controls.Add(this.pB1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.PicNumberTaker, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(666, 373);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // pB1
+            // 
+            this.pB1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.pB1, 2);
+            this.pB1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pB1.Location = new System.Drawing.Point(3, 3);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(660, 329);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB1.TabIndex = 0;
+            this.pB1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.showButton);
+            this.flowLayoutPanel1.Controls.Add(this.clearButton);
+            this.flowLayoutPanel1.Controls.Add(this.BChooseFile);
+            this.flowLayoutPanel1.Controls.Add(this.BGTeinlesen);
+            this.flowLayoutPanel1.Controls.Add(this.closeButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(124, 338);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(539, 32);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // showButton
+            // 
+            this.showButton.AutoSize = true;
+            this.showButton.Location = new System.Drawing.Point(445, 3);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(91, 23);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "Bild anzeigen";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.AutoSize = true;
+            this.clearButton.Location = new System.Drawing.Point(345, 3);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(94, 23);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Anzeige leeren";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // BChooseFile
+            // 
+            this.BChooseFile.AutoSize = true;
+            this.BChooseFile.Location = new System.Drawing.Point(253, 3);
+            this.BChooseFile.Name = "BChooseFile";
+            this.BChooseFile.Size = new System.Drawing.Size(86, 23);
+            this.BChooseFile.TabIndex = 2;
+            this.BChooseFile.Text = "Bilder wählen";
+            this.BChooseFile.UseVisualStyleBackColor = true;
+            this.BChooseFile.Click += new System.EventHandler(this.BChooseFile_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Location = new System.Drawing.Point(65, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "beenden";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // PicNumberTaker
+            // 
+            this.PicNumberTaker.Location = new System.Drawing.Point(3, 338);
+            this.PicNumberTaker.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.PicNumberTaker.Name = "PicNumberTaker";
+            this.PicNumberTaker.Size = new System.Drawing.Size(45, 20);
+            this.PicNumberTaker.TabIndex = 3;
+            // 
+            // BGTeinlesen
+            // 
+            this.BGTeinlesen.AutoSize = true;
+            this.BGTeinlesen.Location = new System.Drawing.Point(146, 3);
+            this.BGTeinlesen.Name = "BGTeinlesen";
+            this.BGTeinlesen.Size = new System.Drawing.Size(101, 23);
+            this.BGTeinlesen.TabIndex = 4;
+            this.BGTeinlesen.Text = "GT Daten wählen";
+            this.BGTeinlesen.UseVisualStyleBackColor = true;
+            this.BGTeinlesen.Click += new System.EventHandler(this.BGTeinlesen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 373);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicNumberTaker)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button BChooseFile;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.NumericUpDown PicNumberTaker;
+        private System.Windows.Forms.Button BGTeinlesen;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    // A struct to handle the Groundtruth data
+    public struct GT
+    {
+        int x_start;
+        int y_start;
+        int x_end;
+        int y_end;
+        int id;
+        public void SetVars(int xs, int ys, int xe, int ye, int idk)
+        {
+            x_start = xs;
+            y_start = ys;
+            x_end = xe;
+            y_end = ye;
+            id = idk;
+        }
+        public int GetVars()
+        {
+            return x_start;
+        }
+    }
+    //A struct to combine GT and the picture
+    public struct picGT
+    {
+       public String picPfadName;
+        public String picName;
+       public GT[] Gtdaten;
+
+        public picGT(string Name)
+        {
+            picName = "";
+            picPfadName = "";
+            Gtdaten = null;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        String[] FileNames = null;
+        picGT[] pictureData = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //Check if our Array was initialized
+            if (FileNames != null)
+            {
+                int picNumSelected = Convert.ToInt32(PicNumberTaker.Value);
+                if ( (FileNames.Length >= picNumSelected) && (FileNames[picNumSelected] != null))
+                {
+                    try
+                    {
+                        pB1.Load(pictureData[picNumSelected].picPfadName);
+                        Bitmap bm = new Bitmap(pB1.Image);
+                        Color pc = new Color();
+                        pc = Color.FromArgb(255, 255, 0, 0);
+
+                        bm.SetPixel(3, 3,pc);
+                        pB1.Image = bm;
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("Error: Couldn't read file!");
+                    }
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Zuerst muss eine Anzahl von Bildern ausgewählt werden.");
+            }
+                       
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pB1.Image = null;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Bilder (*.bmp; *.jpg; *.png)|*.bmp;*.jpg;*.png";
+            //Multiselect
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int numPics = ofd.FileNames.Length;
+                pictureData = new picGT[numPics];
+                for( int i = 0;i <numPics;i++)
+                {
+                    string[] parts = ofd.FileNames[i].Split('\\');
+                    pictureData[i].picPfadName = ofd.FileNames[i];
+                    pictureData[i].picName = parts[parts.Length - 1];
+                }
+
+                FileNames = ofd.FileNames;
+                PicNumberTaker.Maximum = (ofd.FileNames.Length - 1);
+            }
+        }
+
+        private void BGTeinlesen_Click(object sender, EventArgs e)
+        {
+            //Finden der Datei
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Textdateien (*.txt)|*.txt";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string line = null;
+                int counterP = 0;
+                try
+                {
+                    System.IO.StreamReader file = new System.IO.StreamReader(@ofd.FileName);
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        string[] args = line.Split(';');
+                        if (args[0] != "img")
+                        {
+                            while (args[0] != pictureData[counterP].picName && counterP < pictureData.Length)
+                            {
+                                counterP++;
+                            }
+                            if (args[0] == pictureData[counterP].picName && counterP < pictureData.Length)
+                            {
+                                //Keine GT bereits vorhanden
+                                if (pictureData[counterP].Gtdaten == null || pictureData[counterP].Gtdaten[0].GetVars()==0)
+                                {
+                                    pictureData[counterP].Gtdaten = new GT[1];
+                                    pictureData[counterP].Gtdaten[0].SetVars(Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), Convert.ToInt32(args[3]), Convert.ToInt32(args[4]), Convert.ToInt32(args[5]));
+                                }
+                                else// GT vorhanden, also kopieren
+                                {
+                                    GT[] zwischen = pictureData[counterP].Gtdaten;
+                                    pictureData[counterP].Gtdaten = new GT[zwischen.Length + 1];
+                                    //kopieren
+                                    int i;
+                                    for (i = 0; i < zwischen.Length; i++)
+                                    {
+                                        pictureData[counterP].Gtdaten[i] = zwischen[i];
+                                    }
+                                    //Neues hinzufügen
+                                    if (args.Length >= 6)
+                                        pictureData[counterP].Gtdaten[i].SetVars(Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), Convert.ToInt32(args[3]), Convert.ToInt32(args[4]), Convert.ToInt32(args[5]));
+                                    else
+                                        Console.WriteLine("Fehler im Lesen... Zu wenig Argumente");
+                                }
+                            }
+                        }
+                    }
+                    file.Close();
+                    file.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Fehler:Lesen von GT");
+                }
+            }
+          
+        }
+    }
+}
